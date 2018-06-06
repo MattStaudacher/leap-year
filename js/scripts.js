a1db4a48e9ca7eb148578f3063f4fbfec8bd2c1c
Matt Staudacher and Steven Colburn <matt@>
2018-06-06 08:41:38 -0700

    add a check for NaN or negative number for user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 $(function(){
   $("#form-one").submit(function(event){
     event.preventDefault();
-    
+    var year = parseInt($("#input-year").val());
+    if(year === NaN || year < 0)
+    {
+      alert("Please enter a positive year.");
+    }
+    else {
+      {
+        
+      }
+    }
+
   });
 });
