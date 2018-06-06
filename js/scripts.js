1297bef747534408193bbba9e74d1f0ff09a4a58
Matt Staudacher and Steven Colburn <matt@>
2018-06-06 08:55:22 -0700

    added branching to determine if input year is a leap year

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,31 @@ $(function(){
   $("#form-one").submit(function(event){
     event.preventDefault();
     var year = parseInt($("#input-year").val());
-    if(year === NaN || year < 0)
-    {
-      alert("Please enter a positive year.");
-    }
-    else {
-      {
-        
-      }
-    }
+    year = checkForLeapYear(year);
+    outputAnswer(year);
+
 
   });
 });
+
+function outputAnswer(tempString)
+{
+  $("#output").text(tempString);
+}
+
+function checkForLeapYear(year)
+{
+  if(year === NaN)
+  {
+    alert("Please enter a year.");
+    return "";
+  }
+  else if((!(year % 4))&&((year % 100)||(!(year%400))))
+  {
+    return (year.toString() + " is a leap year.");
+  }
+  else
+  {
+    return (year.toString() + " is not a leap year.");
+  }
+}
